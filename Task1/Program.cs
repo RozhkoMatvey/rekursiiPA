7e5c8fecb2275af16bf44e6fc006f34f5616698a
Matvey Rozhko <zopafinikovna@gmail.com>
2023-12-23 20:58:51 +0600

    Добавляю первое задание

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int M = ReadInt("Введите начальное значение M :  ");
+int N = ReadInt("Введите конечное значение N :  ");
+
+void FTask(int N, int M)
+{
+    
+    if (M > N ) return;
+    FTask(N - 1, M);
+    System.Console.Write(N + " ");
+}
+
+FTask(N,M);
+
